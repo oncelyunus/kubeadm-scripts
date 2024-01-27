08ff2dbdd5bccbf535b08593474c7c68efd5b85e
Yunus Oncel <yunusoncel@gmail.com>
2024-01-27 23:46:40 +0300

    .

---
+++ scripts/kube_setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Common setup for kube setup
+# disable swap
+sudo swapoff -a
+
+# keeps the swaf off during reboot
+(crontab -l 2>/dev/null; echo "@reboot /sbin/swapoff -a") | crontab - || true
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+
+sudo apt-get install -y kubelet="1.28.1-00" kubectl="1.28.1-00" kubeadm="1.28.1-00"
+
+# Create the .conf file to load the modules at bootup
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set up required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+#NODENAME=$(hostname -s)
+#POD_CIDR="192.168.0.0/16"
+
+#sudo kubeadm init --control-plane-endpoint=vmi1619987:6443 --apiserver-advertise-address="$NODENAME" --node-name "$NODENAME" --pod-network-cidr="$POD_CIDR"
+
+# Install Claico Network Plugin Network 
+
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+
+#curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
+
+#kubectl create -f custom-resources.yaml
